697cef356a21c911c7f9dae37f9ab04c0b649939
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-12-29 13:32:25 +0530

    animation

--- src/components/Character.js
+++ src/components/Character.js
@@ -45,7 +45,7 @@ const styles = {
             </div> */}
             <div className={charActivate? "char-section": "char-section no-char"}>
                 {/* <button className= {menuHandle===-1? "home-icon animate__animated animate__fadeIn animate__delay-4s":(menuHandle ===0)?"home-icon home-icon-red home-new-pos animate__animated animate__fadeIn animate__delay-4s":(menuHandle %1 ===0)?"home-icon home-icon-red animate__animated animate__fadeIn animate__delay-4s":(menuHandle %1 !==0)?"home-icon animate__animated animate__fadeIn animate__delay-4s": "home-icon no-char"} onClick={menuReset}><img src={home} alt="Main Menu" /></button> */}
-                <button className= {menuHandle===-1? "home-icon icon-animate":(menuHandle ===0)?"home-icon home-icon-red home-new-pos animate__animated animate__fadeIn animate__delay-4s":(menuHandle %1 ===0)?"home-icon home-icon-red animate__animated animate__fadeIn animate__delay-4s":(menuHandle %1 !==0)?"home-icon animate__animated animate__fadeIn animate__delay-4s": "home-icon no-char"} onClick={menuReset}><img src={home} alt="Main Menu" /></button>
+                <button className= {menuHandle===-1? "home-icon icon-animate":(menuHandle ===0)?"home-icon home-icon-red home-new-pos icon-animate":(menuHandle %1 ===0)?"home-icon home-icon-red icon-animate":(menuHandle %1 !==0)?"home-icon icon-animate": "home-icon no-char"} onClick={menuReset}><img src={home} alt="Main Menu" /></button>
                 <Intro menuHandle={menuHandle}/>
                 <Menu menuHandle={menuHandle} setMenuHandle={setMenuHandle}/>
                 <History menuHandle={menuHandle}/>
