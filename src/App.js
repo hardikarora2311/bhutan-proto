b44861f754df8e4795fff3581572b534a62afe8e
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-12-29 13:20:35 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Character from './components/Character';
 import image1 from "./assets/Pema Lingpa 2.png";
-import image2 from "./assets/Supine Demoness 1.png"
-import image3 from "./assets/Guru Rinpoche 1.png"
-import image4 from "./assets/Tibetan Movement 1.png"
+// import image2 from "./assets/Supine Demoness 1.png"
+// import image3 from "./assets/Guru Rinpoche 1.png"
+// import image4 from "./assets/Tibetan Movement 1.png"
 // import { useEffect } from "react";
 
 
@@ -12,9 +12,9 @@ function App() {
   //   document.body.style.zoom = "25%";
   // }, []);
   const char1= [1450,1521, image1,"PEMA LINGPA", 1800, 100]
-  const char2= [800,90, image2,"SUPINE DEMONESS", 44, 1100]
-  const char3= [900,1000, image3,"GURU RINPOCHE", 3800, 100]
-  const char4= [1200,1800, image4,"TIBETAN INFLUX INTO BHUTAN", 2500, 1100]
+  // const char2= [800,90, image2,"SUPINE DEMONESS", 44, 1100]
+  // const char3= [900,1000, image3,"GURU RINPOCHE", 3800, 100]
+  // const char4= [1200,1800, image4,"TIBETAN INFLUX INTO BHUTAN", 2500, 1100]
   return (
     <div className='main'>
       <Character characterInfo={char1}/>
